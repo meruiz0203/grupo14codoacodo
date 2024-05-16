4a75cdc2e8702ef077c10b3eaee02fddeaf09574
SebaBasco <el_basco@hotmail.com>
2024-05-16 19:19:52 -0300

    buscador-index

---
+++ src/js/search.js
@@ -0,0 +1,40 @@
+const button = document.getElementById('search-button')
+let prodId = ''
+
+button.addEventListener('click', async function () {
+  let search = document.getElementById('search').value
+  let array = []
+  const searched = await fetch('../../datos.json')
+    .then((response) => response.json())
+
+    for(let i=0;i<searched.length;i++){
+        if(searched[i].alt.includes(search)){
+        array.push(searched[i])    
+    }
+    }
+    
+    if(array.length > 0){
+        document.getElementById("titulo").style.display = "none"
+        let div_busqueda = document.getElementById("busqueda")
+        let h2_busqueda = document.createElement("h2")
+        h2_busqueda.innerHTML = "Resultados de la búsqueda: "
+        div_busqueda.appendChild(h2_busqueda)
+        for(let i=0;i<array.length;i++){
+            let a = document.createElement("a")
+            a.href = `./src/pages/product-detail.html?prodId=${array[i].id}`
+            let div = document.createElement("div")
+            let img = document.createElement("img")
+            img.style.height="150px"
+            img.src = "src/" + array[i].src.slice(1)
+            div.style.float = "inline-start"
+            img.style.marginLeft = "15px"
+            a.appendChild(img)
+            div.appendChild(a)
+            div_busqueda.appendChild(div)   
+                    
+        }
+    }
+    document.getElementById('search').value = ""
+})
+
+
