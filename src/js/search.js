dec79e1c54c4964ad3307950e59dc376861b8fa7
SebaBasco <el_basco@hotmail.com>
2024-05-17 22:03:35 -0300

    cambios pagina detalle producto

--- src/js/search.js
+++ src/js/search.js
@@ -1,60 +1,58 @@
 const button = document.getElementById('search-button')
 let prodId = ''
+let array = []
 
+document.addEventListener('DOMContentLoaded', async function () {
+  button.addEventListener('click', async function () {
+    let search = document.getElementById('search').value
+    const searched = await fetch('../../datos.json').then((response) =>
+      response.json()
+    )
 
-document.addEventListener('DOMContentLoaded', async function() { 
-button.addEventListener('click', async function () {
-  let search = document.getElementById('search').value
-  let array = []
-  const searched = await fetch('../../datos.json')
-    .then((response) => response.json())
-
-    for(let i=0;i<searched.length;i++){
-        if(searched[i].alt.includes(search)){
-        array.push(searched[i])    
+    for (let i = 0; i < searched.length; i++) {
+      if (searched[i].alt.includes(search)) {
+        array.push(searched[i])
+      }
     }
-    }
-    
-    if(array.length > 0){
-        document.getElementById("titulo").style.display = "none"
-        let div_busqueda = document.getElementById("busqueda")
-        let h2_busqueda = document.createElement("h2")
-        h2_busqueda.innerHTML = "Resultados de la búsqueda: "
-        div_busqueda.appendChild(h2_busqueda)
-        for(let i=0;i<array.length;i++){
-            if(location.pathname === "/src/pages/product.html"){
-            let a = document.createElement("a")
-            a.href = `./product-detail.html?prodId=${array[i].id}`
-            let div = document.createElement("div")
-            let img = document.createElement("img")
-            img.style.width = "150px"
-            img.style.height="150px"
-            img.src = "."+ array[i].src.slice(1)
-            div.style.float = "inline-start"
-            img.style.marginRight = "15px"
-            a.appendChild(img)
-            div.appendChild(a)
-            div_busqueda.appendChild(div)                   
-            }
-            else{
-            let a = document.createElement("a")
-            a.href = `./src/pages/product-detail.html?prodId=${array[i].id}`
-            let div = document.createElement("div")
-            let img = document.createElement("img")
-            img.style.height="150px"
-            img.src = "src/" + array[i].src.slice(1)
-            div.style.float = "inline-start"
-            img.style.marginLeft = "15px"
-            a.appendChild(img)
-            div.appendChild(a)
-            div_busqueda.appendChild(div)   
 
-            }
-                    
+    if (array.length > 0) {
+      document.getElementById('titulo').style.display = 'none'
+      let div_busqueda = document.getElementById('busqueda')
+      div_busqueda.innerHTML= ""
+      let h2_busqueda = document.createElement('h2')
+      h2_busqueda.innerHTML = 'Resultados de la búsqueda: '
+      div_busqueda.appendChild(h2_busqueda)
+      for (let i = 0; i < array.length; i++) {
+        if (location.pathname === '/src/pages/product.html') {
+          let a = document.createElement('a')
+          a.href = `./product-detail.html?prodId=${array[i].id}`
+          let div = document.createElement('div')
+          let img = document.createElement('img')
+          img.style.width = '150px'
+          img.style.height = '150px'
+          img.src = '.' + array[i].src.slice(1)
+          div.style.float = 'inline-start'
+          img.style.marginRight = '15px'
+          a.appendChild(img)
+          div.appendChild(a)
+          div_busqueda.appendChild(div)
+        } else {
+          let a = document.createElement('a')
+          a.href = `./src/pages/product-detail.html?prodId=${array[i].id}`
+          let div = document.createElement('div')
+          let img = document.createElement('img')
+          img.style.height = '150px'
+          img.src = 'src/' + array[i].src.slice(1)
+          div.style.float = 'inline-start'
+          img.style.marginLeft = '15px'
+          a.appendChild(img)
+          div.appendChild(a)
+          div_busqueda.appendChild(div)
         }
+      }
     }
-    document.getElementById('search').value = ""
+    document.getElementById('search').value = ''
+    console.log(array)
     array = []
-})
-
+  })
 })
